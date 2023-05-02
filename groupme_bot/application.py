635370814ae4831da61e1dc6513140ea5ea0bad7
stryker51a <ethanspam3@gmail.com>
2023-05-01 22:45:34 -0500

    fixing import statements

--- groupme_bot/application.py
+++ groupme_bot/application.py
@@ -6,7 +6,7 @@ from starlette.requests import Request
 from starlette.responses import PlainTextResponse, JSONResponse
 from starlette.types import Scope, Receive, Send, ASGIApp
 
-from bot import Bot
+from .bot import Bot
 
 GET = 'GET'
 POST = 'POST'
